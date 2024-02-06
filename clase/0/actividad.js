07108b218b654c4702d2aef26c623d05a01422aa
Joaquin Cejas <joaquin.cejas.2020@gmail.com>
2024-02-05 22:22:12 -0300

    Class 0

---
+++ clase/0/actividad.js
@@ -0,0 +1,15 @@
+let nombre = "Joaco";
+let edad = 29;
+let precio = 150.55;
+let pelis_series = ["Cobra Kai", "Stranger Thinks", "Umbrella Academy"];
+
+console.log(nombre, edad, precio, pelis_series);
+
+//edad++;
+edad += 1;
+//edad = edad + 1;
+
+pelis_series.push("Sex education");
+pelis_series.push("Breaking Bad");
+
+console.log(nombre, edad, precio, pelis_series);
--- clase/0/script.js
+++ clase/0/script.js
@@ -1,1 +1,35 @@
 console.log("Hola Mundo!");
+
+console.log([1, 2 , 3, 4]); //Array
+
+console.log({
+    name: "Joaco",
+    age: 29
+}); //Objeto
+
+// -----------------------------------------------
+//var variable = "Valor"; //Esta declaración ya no se usa
+let variable = "Valor"; //Variable let (Su valor puede modificarse y reasignarse)
+console.log(variable);
+
+variable = "Otro valor";
+console.log(variable);
+
+variable = 123123;
+console.log(variable);
+
+variable = true;
+console.log(variable);
+
+const CONSTANTE = "Valor Constante";
+//CONSTANTE = 123; //No se puede reasignar su valor
+//CONSTANTE += " Otra cosa"; //No se puede añadir otro valor al existente
+
+const Obj = { name: "Joaco" };
+console.log(Obj);
+Obj.course = "Programación Backend";
+console.log(Obj);
+
+//Obj = "Texto"; //Genera error por reasignación de valor
+
+
