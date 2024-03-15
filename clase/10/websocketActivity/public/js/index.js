4545dff90a8bf5b79fb7dc0edcd665e4bb183fd9
Joaquin Cejas <joaquin.cejas.2020@gmail.com>
2024-03-14 22:17:20 -0300

    After Class 3

--- clase/10/websocketActivity/public/js/index.js
+++ clase/10/websocketActivity/public/js/index.js
@@ -2,6 +2,8 @@ const socket = io();
 
 const textInput = document.querySelector("#text-input");
 const showText = document.querySelector("#show-text");
+const chatInput = document.querySelector("#chat-message");
+const allMessages = document.querySelector("#all-messages");
 
 textInput.addEventListener("input", () => {
     socket.emit("message", textInput.value);
@@ -11,3 +13,17 @@ socket.on("messageShow", data => {
     showText.textContent = data;
 });
 
+socket.on("allMessages", data => {
+    let chat = "";
+
+    for (let item of data) {
+        chat += `<b>${item.socketId}:</b> ${item.message}<br>`
+    }
+
+    allMessages.innerHTML = chat;
+});
+
+function send() {
+    socket.emit("chatMessage", chatInput.value);
+    chatInput.value = "";
+}
--- clase/10/websocketActivity/src/app.js
+++ clase/10/websocketActivity/src/app.js
@@ -20,13 +20,27 @@ const httpServer = app.listen(PORT, () => {
     console.log(`Servidor activo en http://localhost:${PORT}`);
 });
 
+//Almacenamiento de mensajes
+const messages = [];
 
 const socketServer = new Server(httpServer);
 socketServer.on("connection", socket => {
     console.log("Nuevo cliente conectado -----> ", socket.id);
 
+    //Mensaje en tiempo real [Parte 1 del ejercicio]
     socket.on("message", data => {
         console.log(data);
         socketServer.emit("messageShow", data) //Emito para todos los usuarios lo que se esta escribiendo
+    });
+
+    //Mensajes de chat [Parte 2 del ejercicio]
+    socket.on("chatMessage", data => {
+        console.log(data);
+        messages.push({
+            socketId: socket.id,
+            message: data
+        });
+
+        socketServer.emit("allMessages", messages);
     });
 });
